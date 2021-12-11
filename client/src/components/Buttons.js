c09b6d172b1f79a98822c893d0d95a0962f1e9c8
edtap14 <edgar.tapia@outlook.es>
2021-12-11 15:26:50 -0600

    [UPDATE]Se crearon los estilos de las tarjetas y la primer vista de todos los estudiantes

---
+++ client/src/components/Buttons.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../sass/Buttons.scss";
+
+export default function Buttons() {
+  return (
+    <div className="butonsDiv">
+      <button className="buttonFilter">ESTUDIANTES</button>
+      <button className="buttonFilter">STAFF</button>
+    </div>
+  );
+}
---
+++ client/src/components/Cards.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../sass/Cards.scss";
+import databaseCharacters from "../json/hp-characters.json";
+
+// console.log(databaseCharacters);
+
+export default function Cards() {
+  return (
+    <div className="divPrincipal">
+      <div className="wrapper">
+        {databaseCharacters.map((c) => {
+          return (
+            <div key={c.name} className="divCards">
+              <div className="divColor">
+                <img
+                  alt="character"
+                  className="imgCharacters"
+                  src={c.image}
+                ></img>
+              </div>
+              <div className="divwhite">
+                <div className="divHead">
+                  <span>{c.alive ? "VIVO" : "FINADO"} </span>
+                  <span>{c.hogwartsStudent ? "/ ESTUDIANTE" : "/ STAFF"}</span>
+                  <button
+                    className="imgFav"
+                    alt="fav"
+                    // back={rectangle1}
+                  ></button>
+                </div>
+                <div className="nameCharacter">
+                  <span>{c.name}</span>
+                </div>
+                <div className="dataCharacter">
+                  <p className="spantitle">
+                    Cumpleaños:
+                    <span> {c.dateOfBirth}</span>
+                  </p>
+                  <p className="spantitle">
+                    Género:
+                    <span>
+                      {" "}
+                      {c.gender.charAt(0).toUpperCase() + c.gender.slice(1)}
+                    </span>
+                  </p>
+                  <p className="spantitle">
+                    Color de ojos:
+                    <span>
+                      {" "}
+                      {c.eyeColour.charAt(0).toUpperCase() +
+                        c.eyeColour.slice(1)}
+                    </span>
+                  </p>
+                  <p className="spantitle">
+                    Color de pelo:
+                    <span>
+                      {" "}
+                      {c.hairColour.charAt(0).toUpperCase() +
+                        c.hairColour.slice(1)}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
