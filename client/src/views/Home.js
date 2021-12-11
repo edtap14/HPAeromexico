8e69c6e7a7aa34fe109d76ccd4906ea586a7c95c
edtap14 <edgar.tapia@outlook.es>
2021-12-11 01:49:19 -0600

    make it better

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../sass/Home.scss";
 import Nav from "../components/Nav";
+import Buttons from "../components/Buttons";
+import Cards from "../components/Cards";
 import harryImg from "../img/Group 1.png";
 
 export default function Home() {
@@ -14,6 +16,12 @@ export default function Home() {
       </div>
       <div className="titlediv">
         <h1 className="title">Selecciona tu filtro</h1>
+      </div>
+      <div className="buttonsDiv">
+        <Buttons />
+      </div>
+      <div>
+        <Cards />
       </div>
       <div className="footerMovil">
         <Nav />
